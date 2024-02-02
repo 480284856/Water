ab99e74a027ad57360b34e332ac5b783f216ddb3
amos <480284856@qq.com>
2024-02-02 23:23:34 +0800

    archive

--- examples/single card multi pricisions/codellama_single.sh
+++ examples/single card multi pricisions/codellama_single.sh
@@ -3,37 +3,39 @@ input_lengths=(16 64 256 1024)
 output_lengths=(8 32 128 512)
 batch_sizes=(1 2 4 8 16)
 
+model_name_or_path="CodeLlama-7b-Instruct-hf"
+device="cuda:0"
 # Assuming the two arrays have the same length
 for j in $(seq 0 $((${#batch_sizes[@]}-1))); do
     for i in $(seq 0 $((${#input_lengths[@]} - 1))); do
                 # fp16
-                python codellama.py \
-                        --model_name_or_path CodeLlama-7b-Instruct-hf \
+                python codellama_single.py \
+                        --model_name_or_path $model_name_or_path \
                         --max_input_length ${input_lengths[$i]} \
                         --max_output_length ${output_lengths[$i]} \
                         --batch_size ${batch_sizes[$j]} \
                         --warm_up_nums 10 \
-                        --device "cuda:0" 
+                        --device $device
                 sleep 3
                 # int 8
-                python codellama.py \
-                        --model_name_or_path CodeLlama-7b-Instruct-hf-param \
+                python codellama_single.py \
+                        --model_name_or_path $model_name_or_path \
                         --max_input_length ${input_lengths[$i]} \
                         --max_output_length ${output_lengths[$i]} \
                         --batch_size ${batch_sizes[$j]} \
                         --warm_up_nums 10 \
-                        --device "cuda:0" \
+                        --device $device \
                         --quantization_type "int8" \
                         --quantized_model_path CodeLlama-7b-Instruct-hf-param-int8 
                 sleep 3
                 # int 4
-                python codellama.py \
-                        --model_name_or_path CodeLlama-7b-Instruct-hf-param \
+                python codellama_single.py \
+                        --model_name_or_path $model_name_or_path \
                         --max_input_length ${input_lengths[$i]} \
                         --max_output_length ${output_lengths[$i]} \
                         --batch_size ${batch_sizes[$j]} \
                         --warm_up_nums 10 \
-                        --device "cuda:0" \
+                        --device $device \
                         --quantization_type "int4" \
                         --quantized_model_path CodeLlama-7b-Instruct-hf-param-int4 
                 sleep 3
